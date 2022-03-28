6bc9edd5be112dd33f12d118a97b22ee3c26e0ff
Ammar Ihsaan <86870047+marssihsaan@users.noreply.github.com>
2022-03-28 15:25:34 +0700

    Tugas web Students CRUD

--- student/src/main/java/id/ac/uin/student/entity/Student.java
+++ student/src/main/java/id/ac/uin/student/entity/Student.java
@@ -1,33 +1,46 @@
 package id.ac.uin.student.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
 @Table(name = "student")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
-    @Column(name = "first_name")
+    @NotEmpty(message = "First name is required")
+    @NotNull(message = "FirstName can not be null!!")
+    @Column(nullable = false, name = "first_name")
     private String firstName;
 
-    @NotEmpty
-    @Column(name = "last_name")
+    @NotEmpty(message = "Last name is required")
+    @NotNull(message = "Last Name can not be null!!")
+    @Column(nullable = false, name = "last_name")
     private String lastName;
 
-    @NotEmpty
-    @Column(name = "email", unique = true)
+    @NotEmpty(message = "email is required")
+    @NotNull(message = "email can not be null!!")
+    @Column(nullable = false, name = "email", unique = true)
     private String email;
 
-    @NotEmpty
-    @Column(name = "phone_number", unique = true)
+    @NotEmpty(message = "phone number is required")
+    @NotNull(message = "phone number can not be null!!")
+    @Column(nullable = false, name = "phone_number", unique = true)
     private String phoneNumber;
 
+    @NotEmpty(message = "jurusan is required")
+    @NotNull(message = "jurusan can not be null!!")
+    @Column(nullable = false, name = "jurusan", unique = true)
+    private String jurusan;
 
 }
